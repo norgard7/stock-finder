c729fa35cfda522d5893a74f83a2f43e971c8f70
Lauren Norgard <lauren.c.schumann@gmail.com>
2021-05-20 21:32:32 -0500

    Gnews API started teh fetch. Grabs 10 articles that are relevant to the topic.

--- pseudo-code/style.js
+++ pseudo-code/style.js
@@ -1,22 +1,39 @@
+let newsEl = document.querySelector("#news");
+let apiKey = "8cd8f664033325a7f14a5b678865218c";
+
+function getNews(company) {
+    let apiUrl = 'https://gnews.io/api/v4/search?q='+ company + '&country=us&token=sortby=relevance&token=' + apiKey;
+    fetch(apiUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
+}
+
+
+
+
 // JS Markup
 
 // Mark const to declare no future change
 
 // Resolve Error Codes: Server Side Calls
-requestUrl = "https:// ..."
+// requestUrl = "https:// ..."
 
-function getApi(requestUrl) {
-    fetch(requestUrl)
-        .then(function (response) {
-            console.log(response.status);
-            //  Conditional for the the response.status.
-            if (response.status) {
-            }
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-        });
-}
+// function getApi(requestUrl) {
+//     fetch(requestUrl)
+//         .then(function (response) {
+//             console.log(response.status);
+//             //  Conditional for the the response.status.
+//             if (response.status) {
+//             }
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data);
+//         });
+// }
 
-getApi(requestUrl);
+// getApi(requestUrl);
