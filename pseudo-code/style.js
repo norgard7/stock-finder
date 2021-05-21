3e9298a7481b3c30eb6e648d29c645aa496c36c5
Austen Rhyce Erickson <a.r.erickson.28@gmail.com>
2021-05-20 21:31:48 -0500

    familiarizing self with api resource

--- pseudo-code/style.js
+++ pseudo-code/style.js
@@ -1,12 +1,25 @@
-// JS Markup
-
-// Mark const to declare no future change
-
 // Resolve Error Codes: Server Side Calls
-requestUrl = "https:// ..."
+// let requestUrl = "https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2020-06-01/2020-06-17?apiKey=kYxMgr9oqrEq239cvWobO1J4q6rbXL6S"
 
-function getApi(requestUrl) {
-    fetch(requestUrl)
+let requestTickers = "https://api.polygon.io/v2/reference/tickers?search=apple&apiKey=kYxMgr9oqrEq239cvWobO1J4q6rbXL6S" ;
+
+//parameters
+tickers = "tickers?"
+
+stocks = "?market=stocks" 
+
+search = "?search=microsoft"
+
+function getApi(requestTickers) {
+
+let companyNames = [] ;
+
+$("#tags").autocomplete({
+    source: companyNames
+})
+
+
+    fetch(requestTickers)
         .then(function (response) {
             console.log(response.status);
             //  Conditional for the the response.status.
@@ -16,7 +29,49 @@ function getApi(requestUrl) {
         })
         .then(function (data) {
             console.log(data);
+
+            let tickerObject = data["tickers"]
+
+            for (let i=0; i < tickerObject.length; i++) {
+                companyNames.push(tickerObject[i]["ticker"])
+            }
+
+            console.log(companyNames) ;
+
         });
 }
 
-getApi(requestUrl);
+getApi(requestTickers);
+
+
+{/* <script>
+  $( function() {
+    var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $( "#tags" ).autocomplete({
+      source: availableTags
+    });
+  } );
+  </script> */}
