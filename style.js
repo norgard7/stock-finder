4976246e194345955417291305377478fb60f709
Austen Rhyce Erickson <a.r.erickson.28@gmail.com>
2021-05-30 16:50:31 -0500

    making some css changes to company info section

--- style.js
+++ style.js
@@ -251,7 +251,7 @@ function getTickers(requestTickers1) {
                         console.log("calling getStockData")
                         getStockData(ticker) ;
                         getStock(ticker); 
-                        getNews(ticker);
+                        // getNews(ticker);
 
                     })
 
